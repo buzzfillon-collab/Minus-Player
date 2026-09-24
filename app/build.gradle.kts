f6e72339ffc6466834feb2b1d91dd24928440470
0xkryo <buzzfillon@gmail.com>
2026-09-24 11:08:08 +0600

    Enable bundled AV1 decoder support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.media3.session)
+    implementation(libs.androidx.media3.decoder.av1)
     implementation(libs.nextlib.media3ext)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
