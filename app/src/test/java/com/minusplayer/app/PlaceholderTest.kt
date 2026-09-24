c988c98834a89d2188e9b853c9ffda7c379bdfaf
0xkryo <buzzfillon@gmail.com>
2026-09-24 09:30:41 +0600

    Add initial unit test

---
+++ app/src/test/java/com/minusplayer/app/PlaceholderTest.kt
@@ -0,0 +1,11 @@
+package com.minusplayer.app
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class PlaceholderTest {
+    @Test
+    fun projectFoundationIsTestable() {
+        assertTrue(true)
+    }
+}
